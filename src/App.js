9f87f1ddd6673e8a56e7535ce3c3c206f68da480
Stephen <stephengift43@gmail.com>
2022-10-23 14:18:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Register from "./Pages/Register/Register";
 import Task from "./Pages/Task/Task";
 import VerifyCode from "./Pages/Verify/VerifyCode";
 import CreateNewTask from "./Pages/CreateNewTask/CreateNewTask";
+import Alltask from "./Pages/Tasklist/Alltask";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
           <Route exact path={"/tasks"} element={<Task />} />
           <Route exact path={"/verify"} element={<VerifyCode />} />
           <Route exact path={"/create-task"} element={<CreateNewTask />} />
+          <Route exact path={"/register"} element={<Register />} />
+          <Route exact path={"/login"} element={<Login />} />
+          <Route exact path={"/tasks/all"} element={<Alltask />} />
         </Routes>
       </Router>
     </div>
