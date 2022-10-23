babb05bf401ff8c161b37c430556f37b4cbf0fec
Stephen <stephengift43@gmail.com>
2022-10-23 12:04:48 +0100

    notification, Create Task, EmailCodeVerification

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Pages/Register/Register";
 import Task from "./Pages/Task/Task";
+import VerifyCode from "./Pages/Verify/VerifyCode";
+import CreateNewTask from "./Pages/CreateNewTask/CreateNewTask";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path={"/"} element={<Register />} />
-        <Route exact path={"/tasks"} element={<Task />} />
-      </Routes>
-    </Router>
+    <div class="container">
+      <Router>
+        <Routes>
+          <Route exact path={"/"} element={<Register />} />
+          <Route exact path={"/tasks"} element={<Task />} />
+          <Route exact path={"/verify"} element={<VerifyCode />} />
+          <Route exact path={"/create-task"} element={<CreateNewTask />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
 export default App;
-
