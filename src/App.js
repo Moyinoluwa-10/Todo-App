65013dc7faf086d73afde75ae1b96b39ed15ef76
Moyinoluwa-10 <moyinadelowo@gmail.com>
2022-10-23 11:43:42 +0100

    Fixed update bug

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Pages/Register/Register";
 import Task from "./Pages/Task/Task";
 import Alltask from "./Pages/Tasklist/Alltask";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path={"/"} element={<Register />} />
-        <Route exact path={"/tasks"} element={<Task />} />
+        <Route exact path={"/register"} element={<Register />} />
+        <Route exact path={"/login"} element={<Login />} />
+        <Route exact path={"/"} element={<Task />} />
         <Route exact path={"/tasks/all"} element={<Alltask />} />
       </Routes>
     </Router>
